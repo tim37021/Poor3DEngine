44ae034f53c4997b6b4ca33c982426bf147b421d
Tim Hsu <tim37021@gmail.com>
2014-08-07 12:38:33 +0800

    Add setEngine function

--- src/Poor3D/Core/Game.h
+++ src/Poor3D/Core/Game.h
@@ -1,15 +1,23 @@
 #ifndef CORE_GAME_H_
 #define CORE_GAME_H_
 
+#include <Poor3D/Core/CoreEngine.h>
+
 namespace Poor3D
 {
 	namespace Core
 	{
+		class CoreEngine;
 		class Game
 		{
 		public:
 			virtual void render() = 0;
 			virtual void update() = 0;
+
+			void setEngine(CoreEngine *e)
+			{ engine = e; }
+		protected:
+			CoreEngine *engine;
 		};
 	}
 }
