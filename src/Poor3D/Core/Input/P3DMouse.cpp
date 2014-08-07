6ed78473bdca786620a75f62a9bc52309ed7c628
Tim Hsu <tim37021@gmail.com>
2014-08-07 12:40:59 +0800

    Complete mouse input and test code

---
+++ src/Poor3D/Core/Input/P3DMouse.cpp
@@ -0,0 +1,38 @@
+#include <Poor3D/Core/Input/P3DMouse.h>
+#include <GLFW/glfw3.h>
+
+using namespace Poor3D::Core::Input;
+using namespace Poor3D::Core::Rendering;
+
+P3DMouse::P3DMouse(P3DWindow *_w)
+	:window(_w)
+{
+
+}
+
+#include <iostream>
+
+void P3DMouse::update()
+{
+	for(int i=0; i<8; i++)
+	{
+		bool state=getButtonState(i);
+
+		down[i] = false;
+		up[i] = false;
+		//if last state is not equal to current state
+		if(current[i] ^ state)
+		{
+			if(state)
+				down[i] = true;
+			else
+				up[i] = true;
+		}
+
+		current[i] = state;
+	}
+}
+bool P3DMouse::getButtonState(int button) const
+{
+	return (glfwGetMouseButton(window->getHandler(), button)==GLFW_PRESS);
+}
--- src/Test/test.cpp
+++ src/Test/test.cpp
@@ -1,10 +1,30 @@
 #include <Poor3D/Core/CoreEngine.h>
+#include <Poor3D/Core/Game.h>
+#include <iostream>
 
 using namespace Poor3D::Core;
 
+class myGame: public Game
+{
+	virtual void update()
+	{
+		if(engine->getMouse()->mouseDown(0)){
+
+			std::cout<<"Mouse Down"<<std::endl;
+		}
+
+		if(engine->getMouse()->mouseUp(0)){
+
+			std::cout<<"Mouse Up"<<std::endl;
+		}
+	}
+	virtual void render(){}
+};
+
 int main(int argc, char *argv[])
 {
-	CoreEngine engine(60.0);
+	myGame *g = new myGame();
+	CoreEngine engine(g);
 
 	engine.createWindow(800, 600, "Default");
 
